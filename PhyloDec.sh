262a53550dbd8789f284e45dbe77239ecc81f576
katherinebraught <katherinebraught@gmail.com>
2020-11-06 13:19:01 -0600

    Add files via upload

---
+++ PhyloDec.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+matrixFile="$1"
+typearg="$2"
+strippedFile="$matrixFile"Stripped
+reducedFile="$matrixFile"Reduced
+
+outputfile="$reducedFile"Results
+resultsFile="$matrixFile"FinalResults
+summaryFile="$matrixFile"Summary.txt
+
+start=`date +%s%N`
+
+python reduce.py -s $matrixFile $strippedFile
+
+python reduce.py -i $strippedFile $reducedFile
+
+python SATreducer.py $reducedFile $typearg
+
+if [ $typearg != "-d" ]
+then
+python reduce.py -o $matrixFile $outputfile $resultsFile
+
+end=`date +%s%N`
+
+#summary file
+python summary.py $summaryFile $matrixFile $strippedFile $reducedFile $outputfile $resultsFile $0$2 $((end-start))
+fi
